4051bb274b3696b2a9c30dd7eabb3b1261afb479
shinychan95 <shinychan95@gmail.com>
2023-07-06 00:21:32 +0900

    MenuBarExtra 사용하여 시스템 메뉴바 앱 기본 기능 구현 (#1)

--- felizeter/felizeterApp.swift
+++ felizeter/felizeterApp.swift
@@ -9,9 +9,59 @@ import SwiftUI
 
 @main
 struct felizeterApp: App {
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("FelizeterApp", image: "TRex") {
+            AppMenu()
+        }
+    }
+}
+
+struct AppMenu: View {
+    @State private var textOutput = "No Action Taken Yet"
+    @State private var isOn = false
+
+    func action1() {
+        textOutput = "Action 1 Performed"
+        print(textOutput)
+    }
+    
+    func action2() {
+        textOutput = "Action 2 Performed"
+        print(textOutput)
+    }
+    
+    func action3() {
+        exit(0)
+    }
+
+    var body: some View {
+        VStack {
+            Text(textOutput)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Button(action: action1, label: { Text("Action 1") })
+            Button(action: action2, label: { Text("Action 2") })
+            
+            Divider()
+
+            Toggle(isOn: $isOn) {
+                Text(isOn ? "Turn OFF" : "Turn ON")
+            }
+            .onChange(of: isOn) { newValue in
+                if newValue {
+                    print("ON, start measuring system metrics...")
+                    // Insert code here to start measuring system metrics
+                } else {
+                    print("OFF, stop measuring system metrics...")
+                    // Insert code here to stop measuring system metrics
+                }
+            }
+            
+            Divider()
+            
+            Button(action: action3, label: { Text("Exit") })
         }
     }
 }
