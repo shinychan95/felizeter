ff57d5d8dfcb847246c3e34f508338032d9da412
shinychan95 <shinychan95@gmail.com>
2023-07-05 22:56:56 +0900

    Initial Commit (#1)

---
+++ felizeter/felizeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  felizeterApp.swift
+//  felizeter
+//
+//  Created by USER on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct felizeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
